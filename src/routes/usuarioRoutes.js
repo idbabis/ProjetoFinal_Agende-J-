171265c436974f3cfa29a238311beff8bf395041
idbabis <idbabis@gmail.com>
2022-07-29 00:21:22 -0300

    atualizei rotas

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -7,7 +7,7 @@ router.post("/usuario/", controller.registerNewUsuario);
 router.post("/login", controller.login)
 router.get("/usuarios",auth, controller.todosUsuarios)
 router.get("/lojas",auth, controller.findAllLojas);
-router.patch("/alterar/:id",auth, controller.alterarUsuario);
-router.delete("/deletar/:id", controller.deletarUsuario);
+router.patch("/usuario/:id",auth, controller.alterarUsuario);
+router.delete("/usuario/:id", controller.deletarUsuario);
 
 module.exports = router;
